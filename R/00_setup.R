115ee441c9878818ae98d72c654f65e468f0cfcf
theodoretobel <theotobel@gmail.com>
2026-05-09 15:21:38 -0400

    fixed ggradar install

--- R/00_setup.R
+++ R/00_setup.R
@@ -11,7 +11,7 @@ packages <- c(
   "car",
   "purrr",
   "broom",
-  "ggradar",
+  "remotes",
   "ggdist",
   "ggsignif",
   "ggpubr",
@@ -25,8 +25,17 @@ if (any(!installed)) {
   install.packages(packages[!installed], repos = "https://cloud.r-project.org/")
 }
 
+# Update fs to fix version conflicts
+install.packages("fs", repos = "https://cloud.r-project.org/")
+
+# Install ggradar from GitHub if needed
+if (!require("ggradar", quietly = TRUE)) {
+  remotes::install_github("ricardo-bion/ggradar")
+}
+
 # Load all packages
 lapply(packages, library, character.only = TRUE)
+library(ggradar)
 
 # Set options
 set.seed(420)           # Reproducibility in bootstrapping
